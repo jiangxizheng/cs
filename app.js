a6108220fe9ccc4436c63556e1e7aa74d12008c7
jiangxizheng <3262704574@163.com>
2023-04-08 10:14:25 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// app.js
+import $http from "./api/index";
+App({
+  onLaunch() {
+    // 展示本地存储能力
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+  },
+  globalData: {
+    userInfo: null,
+    name:'上海'
+  }
+})
